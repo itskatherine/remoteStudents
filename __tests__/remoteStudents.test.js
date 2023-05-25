aad2dabf211bf1b45f5875f85372968be1941968
Katherine <hurstkatherine@hotmail.co.uk>
2023-05-25 14:30:47 +0100

    add solution

--- __tests__/remoteStudents.test.js
+++ __tests__/remoteStudents.test.js
@@ -1,3 +1,56 @@
 const updateRemoteStudents = require("../remoteStudents");
 
-describe("updateRemoteStudents", () => {});
+describe("updateRemoteStudents", () => {
+  test("should return empty array if passed empty array", () => {
+    expect(updateRemoteStudents([])).toEqual([]);
+  });
+  test("should return a new array", () => {
+    const inputArray = [];
+    const output = updateRemoteStudents(inputArray);
+    expect(inputArray).not.toBe(output);
+  });
+  test("when passed a array with one object with a location property, return an equal array", () => {
+    const input = [{ name: "Hypatia", age: 31, location: "leeds" }];
+    const expected = [{ name: "Hypatia", age: 31, location: "leeds" }];
+    const actual = updateRemoteStudents(input);
+    expect(actual).toEqual(expected);
+  });
+  test("should when passed an array with one object without a location, updates students location with remote", () => {
+    const input = [{ name: "Ramanujan", age: 22 }];
+    const expected = [{ name: "Ramanujan", age: 22, location: "remote" }];
+    const actual = updateRemoteStudents(input);
+    expect(actual).toEqual(expected);
+  });
+  test("when passed an array with multiple studes of mixed locations, returns array where students with no locations have remote", () => {
+    const input = [
+      { name: "Hypatia", age: 31, location: "leeds" },
+      { name: "Ramanujan", age: 22 },
+      { name: "Katherine", age: 30 },
+      { name: "Hev", age: 27, location: "Manchester" },
+    ];
+    const expected = [
+      { name: "Hypatia", age: 31, location: "leeds" },
+      { name: "Ramanujan", age: 22, location: "remote" },
+      { name: "Katherine", age: 30, location: "remote" },
+      { name: "Hev", age: 27, location: "Manchester" },
+    ];
+    const actual = updateRemoteStudents(input);
+    expect(actual).toEqual(expected);
+  });
+  test("should not mutate the passed in array", () => {
+    const input = [
+      { name: "Hypatia", age: 31, location: "leeds" },
+      { name: "Ramanujan", age: 22 },
+      { name: "Katherine", age: 30 },
+      { name: "Hev", age: 27, location: "Manchester" },
+    ];
+    const inputCopy = [
+      { name: "Hypatia", age: 31, location: "leeds" },
+      { name: "Ramanujan", age: 22 },
+      { name: "Katherine", age: 30 },
+      { name: "Hev", age: 27, location: "Manchester" },
+    ];
+    updateRemoteStudents(input);
+    expect(input).toEqual(inputCopy);
+  });
+});
--- remoteStudents.js
+++ remoteStudents.js
@@ -9,20 +9,23 @@ updateRemoteStudents([
   { name: 'Hypatia', age: 31, location: 'leeds' },
   { name: 'Ramanujan', age: 22 }
 ]);
-should return
+//should return
 [
   { name: 'Hypatia', age: 31, location: 'leeds' },
-  { name: 'Ramanujan', age: 22, location: 'remote' },
+  { name: 'Ramanujan', age: 22, location: 'remote' }
 ];
 */
 
-const updateRemoteStudents = () => {};
+const updateRemoteStudents = (array) => {
+  const updatedStudents = array.map((student) => {
+    const studentCopy = { ...student };
+    if (!student.hasOwnProperty("location")) {
+      studentCopy.location = "remote";
+    }
+    return studentCopy;
+  });
 
-
-
-
-
-
-
+  return updatedStudents;
+};
 
 module.exports = updateRemoteStudents;
