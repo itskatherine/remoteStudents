8d19611e3546eb825b9f1edfd4b9b178822c51ad
Katherine <hurstkatherine@hotmail.co.uk>
2023-05-25 13:55:50 +0100

    add description

--- __tests__/remoteStudents.test.js
+++ __tests__/remoteStudents.test.js
@@ -1,3 +1,3 @@
-const remoteStudents = require("../remoteStudents");
+const updateRemoteStudents = require("../remoteStudents");
 
-describe("remoteStudents", () => {});
+describe("updateRemoteStudents", () => {});
--- remoteStudents.js
+++ remoteStudents.js
@@ -1,3 +1,28 @@
-const remoteStudents = () => {};
+/*
+take an array of student objects
+some students have a specified location property
+our function should return a new array
+where all student objects without a location property
+now have a location property of "remote"
 
-module.exports = remoteStudents;
+updateRemoteStudents([
+  { name: 'Hypatia', age: 31, location: 'leeds' },
+  { name: 'Ramanujan', age: 22 }
+]);
+should return
+[
+  { name: 'Hypatia', age: 31, location: 'leeds' },
+  { name: 'Ramanujan', age: 22, location: 'remote' },
+];
+*/
+
+const updateRemoteStudents = () => {};
+
+
+
+
+
+
+
+
+module.exports = updateRemoteStudents;
